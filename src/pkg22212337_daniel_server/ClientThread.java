8622eed454e35276ebe9160971aacebe30ea3362
Danielggz <dggz1993@gmail.com>
2024-10-13 13:00:35 +0100

    Multithread connection with clients

--- src/pkg22212337_daniel_server/ClientThread.java
+++ src/pkg22212337_daniel_server/ClientThread.java
@@ -33,13 +33,15 @@ public class ClientThread implements Runnable{
         {
             while(!message.equalsIgnoreCase("STOP")){
                 BufferedReader in = new BufferedReader( new InputStreamReader(link.getInputStream()));
-            
-                //Receive message from the client
-                message = in.readLine();
-
+                //Receive message from the client and check if is not STOP
                 //Message is add
-                addMethod(message);
+                message = in.readLine();
+                add(message); 
                 //Message is list
+                //Message is STOP
+                if(message.equalsIgnoreCase("STOP")){
+                    stop();
+                }
             }
          }
         catch(IOException e)
@@ -60,15 +62,24 @@ public class ClientThread implements Runnable{
         }
     }
     
-    public void addMethod(String message){
+    public void add(String message){
         try{
             PrintWriter out = new PrintWriter(link.getOutputStream(),true);
             //Receives the message
-            System.out.println("The message in server: " + message);
+            System.out.println("The message from " + name + " in server: " + message);
             out.println("Echo Message for client: " + message); 
         }catch(IOException e){
             System.out.println("Error when printing message: " + e.getMessage());
         }
-        
+    }
+    
+    public void stop(){
+        try{
+            PrintWriter out = new PrintWriter(link.getOutputStream(),true);
+            //Send stop request
+            out.println("Closing..." + name); 
+        }catch(IOException e){
+            System.out.println("Error when printing message: " + e.getMessage());
+        }
     }
 }
